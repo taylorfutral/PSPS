11be2a1d430fe39c8481fb2130f5e507aba27785
Karen Chiao <kchiao@berkeley.edu>
2019-06-04 22:55:25 -0700

    Adds string consumer

---
+++ KafkaProducer/src/main/java/GeneralConsumer/GeneralConsumer.java
@@ -0,0 +1,63 @@
+package KafkaProducer:
+
+import java.util.Properties;
+import java.util.Arrays;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class GeneralConsumer {
+    private static final String BOOTSTRAP_SERVERS = 
+    		"localhost:9092";
+
+   public static void main(String[] args) throws Exception {
+      if(args.length == 0){
+         System.out.println("Enter topic name");
+         return;
+      }
+      //Kafka consumer configuration settings
+      String topicName = args[0].toString();
+      Properties props = new Properties();
+      
+      props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+      props.put("group.id", "test");
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+      props.put("session.timeout.ms", "30000");
+      props.put("key.deserializer", 
+         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+      props.put("value.deserializer", 
+         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+      KafkaConsumer<String, String> consumer = new KafkaConsumer
+         <String, String>(props);
+      
+      //Return list of available topics to choose from 
+	  if (topicName == "--list") {
+	     Map<String, List<PartitionInfo> topics = consumer.listTopics();
+	     System.out.println("Available topics:");
+	     for (Map.Entry<String, List<PartitionInfo> entry : topics) {
+	     	System.out.println("> " + entry.getKey());
+	     }
+	     System.out.println("\nEnter topic name");
+	     return;
+	  }
+      
+      //Kafka Consumer subscribes list of topics here.
+      consumer.subscribe(Arrays.asList(topicName))
+      
+      //print the topic name
+      System.out.println("Subscribed to topic " + topicName);
+      int i = 0;
+      
+      while (true) {
+         ConsumerRecords<String, String> records = consumer.poll(100);
+         for (ConsumerRecord<String, String> record : records) {
+         
+         	// print the offset,key and value for the consumer records.
+         	System.out.printf("offset = %d, key = %s, value = %s\n", 
+            		record.offset(), record.key(), record.value());
+         }
+      }
+   }
+}
